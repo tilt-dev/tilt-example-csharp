26e0f8eaa3314f6128c23f15f3b73b73e2a06375
Maia <maia.mcc@gmail.com>
2020-05-15 19:04:08 -0400

    use restart_process extension

--- 3-live-update/hello-tilt/Pages/Index.cshtml.cs
+++ 3-live-update/hello-tilt/Pages/Index.cshtml.cs
@@ -11,7 +11,7 @@ namespace hello_tilt.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private const long startTimeMillis = 1589398670816;
+        private const long startTimeMillis = 1589583709681;
         private static DateTime end = DateTime.Now;
 
         public IndexModel(ILogger<IndexModel> logger)
