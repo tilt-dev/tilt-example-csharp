1e338116fab809d316384681d3426a22bc57adf8
Maia McCormick <maia@windmill.engineering>
2021-05-07 13:48:18 -0400

    bump restart process ext (#11)

--- 3-live-update/hello-tilt/Pages/Index.cshtml.cs
+++ 3-live-update/hello-tilt/Pages/Index.cshtml.cs
@@ -11,7 +11,7 @@ namespace hello_tilt.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private const long startTimeMillis = 1589583709681;
+        private const long startTimeMillis = 1619813943010;
         private static DateTime end = DateTime.Now;
 
         public IndexModel(ILogger<IndexModel> logger)
